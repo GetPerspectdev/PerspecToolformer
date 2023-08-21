54946647908f2acba4f86632914a15255562efca
IMJONEZZ <46764336+IMJONEZZ@users.noreply.github.com>
2023-08-21 08:56:37 -0600

    One chain runner

--- test_app.py
+++ test_app.py
@@ -5,6 +5,7 @@ from langchain.chains import LLMChain, LLMMathChain, TransformChain, SequentialC
 from langchain.chains import ConversationChain
 from langchain.chains.conversation.memory import  ConversationSummaryBufferMemory, ConversationBufferWindowMemory
 
+import gradio as gr
 import json
 
 
@@ -25,7 +26,7 @@ Thinking through this step-by-step, you can improve your toxicity by"""
 toxic_prompt_template = PromptTemplate(input_variables=['messages'], template=toxic_template)
 
 llm = LlamaCpp(
-    model_path="./models/hermes-llongma-2-7b-8k.ggmlv3.q2_K.bin",
+    model_path="./models/hermes-llongma-2-7b-8k.ggmlv3.q5_K_M.bin",
     n_gpu_layers=0,
     n_batch=512,
     n_ctx=8000,
@@ -36,16 +37,18 @@ toxicity_chain = LLMChain(llm=llm, prompt=toxic_prompt_template)
 
 number_template = """Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
 ###Instruction:
-You are an expert witness specializing in toxicity and empathy.
-Given a summary from another expert, rate the messages on a scale of 1-100 for how toxic they are.
-Please respond with only an integer between 1 and 100.
+You are an expert witness specializing in toxicity and professionalism.
+Given a person's message history, rate the messages on a scale of 1-100 for how professional they are.
+Please respond first with only an integer between 1 and 100, where 1 is always toxic and 100 is always professional.
+Then add an example on how to improve.
 
 ###Input:
-An expert previously said this: {synopsis}
+Message History:
+{synopsis}
 
 
 ###Response:
-Your Toxicity rating from 1-100 is """
+Your professionalism rating from 1-100 is """
 
 number_prompt_template = PromptTemplate(input_variables=["synopsis"], template=number_template)
 
@@ -53,14 +56,34 @@ review_chain = LLMChain(llm=llm, prompt=number_prompt_template)
 
 # This is the overall chain where we run these two chains in sequence.
 
-overall_chain = SimpleSequentialChain(chains= [toxicity_chain, review_chain],
-                                      verbose=True)
+#overall_chain = SimpleSequentialChain(chains= [toxicity_chain, review_chain], verbose=True)
 
 
 
 
 
 def predict_convo(input=""):
-    toxicity = overall_chain.run(input)
-    obj = json.dumps({"toxicity": toxicity}, indent=4)
+    professionalism = review_chain.run(input)
+    obj = json.dumps({"professionalism": professionalism}, indent=4)
     return obj
+
+with gr.Blocks() as app:
+    messages = gr.Textbox(label="messages")
+
+    gr.Examples(
+        [
+            ["Brent:You're the best!\nJake:We can do it.\nBrent:You've never been good enough.\nJake:I can take care of that whenever.\nBrent:sup bro\nJake:How do we know when we're done?\nBrent:Are you going to be in the office today?\nJake:How long does it take to get there?\nBrent:I'll be there in about 30 minutes\nJake:Why are you always constantly late?"],
+        ],
+        inputs=[messages]
+    )
+
+    start_btn = gr.Button("Detect Professionalism")
+    start_btn.click(
+        fn=predict_convo,
+        inputs=[messages],
+        outputs=gr.Textbox(label="output"),
+        api_name="professionaldetect"
+    )
+    
+app.queue(concurrency_count=1)
+app.launch()#inbrowser=True, share=True,
